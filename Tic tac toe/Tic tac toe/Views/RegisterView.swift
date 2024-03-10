0199dc90353f2042c4876cab1f80e3c6fa07a4f0
Jan Kazubski <jkazubski@icloud.com>
2024-03-10 19:29:53 +0100

    Implemented RegisterView

--- Tic tac toe/Tic tac toe/Views/RegisterView.swift
+++ Tic tac toe/Tic tac toe/Views/RegisterView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var email = ""
+    @State private var password = ""
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                //TODO: load image
+            } label: {
+                Image(systemName: "person.fill").font(.system(size: 90)).padding()
+                .foregroundColor(.black)
+            }
+            .overlay(RoundedRectangle(cornerRadius: 64).stroke(Color.black, lineWidth: 3))
+            
+            Group {
+                TextField("email", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                SecureField("password", text: $password)
+            }
+            .padding(12)
+            .background(.white)
+            .cornerRadius(5)
+            
+            VStack {
+                Button {
+                    // TODO: create new account
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("create_account").foregroundColor(.white).padding().bold()
+                        Spacer()
+                    }
+                    .background(.blue).cornerRadius(5)
+                }
+            }.padding()
+        }
+        .padding()
     }
 }
 
-#Preview {
+#Preview("English") {
     RegisterView()
+        .environment(\.locale, Locale(identifier: "EN"))
+}
+
+#Preview("Polish") {
+    RegisterView()
+        .environment(\.locale, Locale(identifier: "PL"))
 }
