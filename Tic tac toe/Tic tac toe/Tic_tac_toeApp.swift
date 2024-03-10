6f65f29b31ff394b4159958427de0e88ad0a47bd
Jan Kazubski <jkazubski@icloud.com>
2024-03-10 19:05:49 +0100

    Created Swift project

---
+++ Tic tac toe/Tic tac toe/Tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_tac_toeApp.swift
+//  Tic tac toe
+//
+//  Created by Jan Kazubski on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
