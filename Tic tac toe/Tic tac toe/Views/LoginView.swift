d1469f31466ad47c33ed8e11862cc275ce366ab4
Jan Kazubski <jkazubski@icloud.com>
2024-03-10 19:21:16 +0100

    Implemented ContentView, added empty classes and added translations

---
+++ Tic tac toe/Tic tac toe/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Tic tac toe
+//
+//  Created by Jan Kazubski on 10/03/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Tic tac toe/Tic tac toe/Views/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Tic tac toe
+//
+//  Created by Jan Kazubski on 10/03/2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
